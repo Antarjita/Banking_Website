a06ec2d2d46535b7c6f782809a0964fcd938e728
Antarjita Mandal <43675459+Antarjita@users.noreply.github.com>
2021-05-31 17:21:43 +0530

    Add files via upload

---
+++ final/Rough.sql
@@ -0,0 +1,7 @@
+UPDATE customer SET Name="anjali2" where Name="anjali";
+select * from customer;
+
+UPDATE customer SET Name="anjali" where Cust_ID=1;
+
+DELETE FROM customer WHERE Cust_ID>4;
+DELETE FROM account WHERE Owner_id>4;
